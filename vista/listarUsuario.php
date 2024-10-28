a090c244c75809da0e4a554f6257064b49648302
Balenbv <valentinbv29@gmail.com>
2024-10-28 20:06:02 -0300

    Refactorizar vista listarUsuario.php para mejorar legibilidad y buenas prácticas

--- vista/listarUsuario.php
+++ vista/listarUsuario.php
@@ -1,28 +1,31 @@
 <?php 
 include_once '../config.php';
 
-echo "<h1>Nombre de los Usuarios</h1>";
 
 $abmUsuario = new abmUsuario();
-$usuarios =  $abmUsuario->obtenerDatos(null);
+echo $abmUsuario->buscar(null);
 
     if (isset($usuarios)){
         foreach ($usuarios as $usuario) {
-            echo "Nombre: ".$usuario['usNombre']."<br>";
-            echo "Mail: ".$usuario['usMail']."<br>";
-            echo "Deshabilitado: ".$usuario['usDeshabilitado']."<br>";
-            echo "<br>";
+            
+            $usuarioId = $usuario->getId();
+            $usNombre = $usuario->getUsNombre();
+            $uspass = $usuario->getUsPass();
+            $usMail = $usuario->getUsuario();
+            echo $usuarioId."\n";
+            echo $usNombre.'\n';
+            echo $uspass.'\n';
+            echo $usMail.'\n';
         }
-
+        
     } else {
         echo "no hay nada";
     }
 
-    echo "<br>";
     echo "<a href='./index.php'><button>Volver al index</button></a>";
-    echo "<a href='./registrarUsuario.php'><button>Registrar Usuario</button></a>";
-    echo "<a href='./formActualizarUsuario.php'><button>actualizar datos del usuario</button></a>";
-    echo "<a href='./formBorrarUsuario.php'><button>eliminar datos del usuario</button></a>";
+    echo "<a href='./login.php'><button>Registrar Usuario</button></a>";
+    echo "<a href='./Action/actualizarLogin.php'><button>actualizar datos del usuario</button></a>";
+    echo "<a href='./Action/eliminarLogin.php'><button>eliminar datos del usuario</button></a>";
 
     
 ?>
